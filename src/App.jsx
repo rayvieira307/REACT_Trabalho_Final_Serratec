85fecdd9a25ce063690715fcc8c1aab590895b7a
Gouiid <guimbl@hotmail.com>
2024-11-02 15:44:29 -0300

    Criação das rotas, Header, Footer e Login(Design)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AppRoute from "./routes/AppRoute";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+        <AppRoute/>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
-
