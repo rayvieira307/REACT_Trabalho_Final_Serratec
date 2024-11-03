b70c8548acd83679916167e1f43498354d5f6743
n4th05 <n4th.machado@gmail.com>
2024-11-03 18:51:43 -0300

    Post criado

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import * as styles from "./Post.module.css";
+import { Link } from "react-router-dom";
 
 export default function Post() {
   return (
-    <div>
-        
-    </div>
-  )
+    <main>
+      <div className={styles.container}>
+        <div className={styles.formulario}>
+
+          <form>
+            <div className={styles.tituloInput}>
+              <label>Título</label>
+              <input type="text" name="titulo" />
+            </div>
+            <div className={styles.conteudoInput}>
+              <label>Conteúdo</label>
+              <textarea type="text" name="conteudo" cols={30} rows={10}/>
+            </div>
+          </form>
+
+          <Link to={"/home"}>
+            <button className={styles.buttonEnviar}>Enviar</button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
