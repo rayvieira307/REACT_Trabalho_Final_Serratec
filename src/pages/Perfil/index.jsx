e22b44127932e6311fb05a3e3a11f96cf7e37eea
Rayssa <vieirarayssa167@gmail.com>
2024-11-04 12:52:39 -0300

    Pagina Perfil

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import * as styles from "./Perfil.module.css";
 
 export default function Perfil() {
   return (
-    <div>
-        
-    </div>
-  )
+    <main>
+      <div className={styles.container}> 
+        <div className= {styles.perfil}>
+           <div className={styles.foto_usuario}>
+             <img src="" alt="" />
+           </div>
+         <h3>Nome do Usuario</h3>
+         </div>
+            <div className={styles.posts}>
+            <div className= {styles.post_card}>A POSTAGEM DO USUÁRIO</div>
+            <div className= {styles.post_card}>A POSTAGEM DO USUÁRIO</div>
+          </div> 
+        </div>  
+     </main>
+  
+
+  )   
 }
