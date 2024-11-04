cfd121d43bdff7ba0fe0601715b5abacbd2527f3
Rayssa <vieirarayssa167@gmail.com>
2024-11-04 17:17:03 -0300

    Icone redirecionando pro perfil e Nova Logo implementada

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import * as styles from "./Header.module.css"
-import Logo from "../../assets/Logo_Header.svg"
-
+import Logo from "../../assets/LogoNova_Header.png"
+import Perfil from "../../assets/Perfil_Header.svg"
 export default function Header() {
   return (
     <header className={styles.cabecalho}>
         <img src={Logo} alt="logo" className={styles.logo}/>
         <h1>PlayConnect</h1>
+        <a href="/perfil"><img src={Perfil} alt="perfil" className={styles.perfil}/></a> 
+       
     </header>
+        
   )
 }
