b4bd02b7fe6a8b25f945b95bbcf8b524585661af
Gouiid <guimbl@hotmail.com>
2024-11-05 22:21:13 -0300

    grandes atualizações

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import * as styles from "./Perfil.module.css";
+import axios from "axios";
+import { Link } from "react-router-dom/dist";
 
 export default function Perfil() {
-  return (
-    <main>
-      <div className={styles.container}> 
-        <div className= {styles.perfil}>
-           <div className={styles.foto_usuario}>
-             <img src="" alt="" />
-           </div>
-         <h3>Nome do Usuario</h3>
-         </div>
-            <div className={styles.posts}>
-            <div className= {styles.post_card}>A POSTAGEM DO USUÁRIO</div>
-            <div className= {styles.post_card}>A POSTAGEM DO USUÁRIO</div>
-          </div> 
-        </div>  
-     </main>
-  
+  const [posts, setPosts] = useState([]);
+  const token = localStorage.getItem("token");
+  const username = localStorage.getItem("username");
 
-  )   
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/post", {
+        headers: {
+          Authorization: { token },
+        },
+      })
+      .then((res) => {
+        const lista = res.data.filter(
+          (item) => item.postUsuarioDTO.email == username
+        );
+        setPosts(lista);
+        console.log(res.data);
+      });
+  }, []);
+
+  function apagar(id){
+    axios.delete(`http://localhost:8080/post/${id}`)
+    .then(() => {
+      console.log("Post apagado!!!");
+      setPosts(posts.filter((post) => post.id!==id))
+    })
+}
+
+  return (
+    <main className={styles.container}>
+        <div className={styles.perfil}>
+          <div className={styles.foto_usuario}></div>
+          <h3>{username}</h3>
+        <button className={styles.btnAtualizarPerfil}>Atualizar</button>
+        </div>
+        <div className={styles.post}>
+          {posts.map((post, key) => (
+            <div className={styles.containerCard}>
+              <div className={styles.post_card}>
+                <p>{post.conteudo}</p>
+              </div>
+              <Link to={`/atualizar/${post.id}`}>
+              <button className={styles.btnAtualizar}>Atualizar</button>
+              </Link>
+              <button className={styles.btnDeletar} onClick={() => apagar(post.id)}>Deletar</button>
+              </div>
+          ))}
+        </div>
+    </main>
+  );
 }
